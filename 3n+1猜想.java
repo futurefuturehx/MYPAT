967874e9669464b323bbcf6750f7276f847fb693
futurefuturehx <360574435@qq.com>
2017-11-22 20:12:45 +0800

    3n+!猜想
    对任何一个自然数n，如果它是偶数，那么把它砍掉一半；如果它是奇数，那么把(3n+1)砍掉一半。这样一直反复砍下去，最后一定在某一步得到n=1。卡拉兹在1950年的世界数学家大会上公布了这个猜想，传说当时耶鲁大学师生齐动员，拼命想证明这个貌似很傻很天真的命题，结果闹得学生们无心学业，一心只证(3n+1)，以至于有人说这是一个阴谋，卡拉兹是在蓄意延缓美国数学界教学与科研的进展……
    我们今天的题目不是证明卡拉兹猜想，而是对给定的任一不超过1000的正整数n，简单地数一下，需要多少步（砍几下）才能得到n=1？
    
    输入格式：每个测试输入包含1个测试用例，即给出自然数n的值。
    输出格式：输出从n计算到1需要的步数。

--- 3n+1猜想.java
+++ 3n+1猜想.java
@@ -4,12 +4,16 @@ public class 猜想{
 	public static void main(String[]args){
 		Scanner in=new Scanner(System.in);
 		//nextInt只读取整数类型 焦点不会换行
-		next()读取输入直到空格 读取之后 焦点不会换行
-		nextLine()读取包括空格 读取之后 焦点会换行
+		//next()读取输入直到空格 读取之后 焦点不会换行
+		//nextLine()读取包括空格直到换行 焦点会换行
 		int number=in.nextInt();
+		//Scanner的close()是关闭输入流 释放空间
 		in.close();
+		//这边的int cnt=0的cnt变量是计数器 用来统计除以二的次数
 		int cnt=0;
+		//这边while (number!=1)是让不断循环除直到等于1
 		while(number!=1){
+			//这边if(number%2！=0)是指如果不是偶数
 			if(number%2!=0){
 				number=3*number+1;
 			}
